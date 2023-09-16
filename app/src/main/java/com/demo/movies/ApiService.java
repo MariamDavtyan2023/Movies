0e08d56fb18cf82fecb7d5ef71c2ecab289b0c0b
MariamDavtyan2023 <davtyan_mariam91@mail.ru>
2023-09-16 14:46:48 +0400

    Update api

--- app/src/main/java/com/demo/movies/ApiService.java
+++ app/src/main/java/com/demo/movies/ApiService.java
@@ -1,17 +1,25 @@
 package com.demo.movies;
 
 import io.reactivex.rxjava3.core.Single;
+import retrofit2.http.Field;
 import retrofit2.http.GET;
+import retrofit2.http.HEAD;
+import retrofit2.http.HTTP;
+import retrofit2.http.Header;
+import retrofit2.http.HeaderMap;
+import retrofit2.http.Path;
 import retrofit2.http.Query;
+import retrofit2.http.QueryMap;
+import retrofit2.http.QueryName;
 
 public interface ApiService {
 
-    @GET("movie?token=MEB886V-E2W41M8-HCR6N80-5E199Q9&field=rating.imdb&search=7-10&sortField=votes.imdb&sortType=-1&limit=30")
+    @GET("v1.3/movie?token=MEB886V-E2W41M8-HCR6N80-5E199Q9&field=rating.imdb&search=7-10&sortField=votes.imdb&sortType=-1&limit=30")
     Single<MovieResponse> loadMovies(@Query("page") int page);
 
-    @GET("movie?token=MEB886V-E2W41M8-HCR6N80-5E199Q9&field=id")
-    Single<TrailerResponse> loadTrailers(@Query("search") int id);
+    @GET("v1.3/movie/{id}?token=MEB886V-E2W41M8-HCR6N80-5E199Q9")
+    Single<TrailerResponse> loadTrailers(@Path("id") int id);
 
-    @GET("review?token=MEB886V-E2W41M8-HCR6N80-5E199Q9&field=movieId&limit=10")
-    Single<ReviewResponse> loadReviews(@Query("search") int id);
+    @GET("v1/review?token=MEB886V-E2W41M8-HCR6N80-5E199Q9")
+    Single<ReviewResponse> loadReviews(@Query("movieId") int id);
 }
